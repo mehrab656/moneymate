64d94771d0ff2e7ba015700ce167e63f3a5935c8
mehrab656 <hossainmehraab@gmail.com>
2023-12-21 22:04:08 +0400

    Initialize project on new laptop

--- backendApi/app/Http/Kernel.php
+++ backendApi/app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        ContentSecurityPolicy::class
+        ContentSecurityPolicy::class,
     ];
 
     /**
@@ -44,6 +44,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Cors::class,//cors added here
         ],
     ];
 
--- backendApi/app/Http/Middleware/ContentSecurityPolicy.php
+++ backendApi/app/Http/Middleware/ContentSecurityPolicy.php
@@ -15,6 +15,7 @@ class ContentSecurityPolicy
      */
     public function handle(Request $request, Closure $next): Response
     {
+
         $response = $next($request);
 
 //        $response->header(
