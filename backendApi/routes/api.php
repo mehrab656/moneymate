0c4539f873fb8ced8d116cd7533a3dacdcc2dada
azmir849 <commongithub@gmail.com>
2024-02-09 23:09:29 +0600

    filter oever all report

--- backendApi/routes/api.php
+++ backendApi/routes/api.php
@@ -38,7 +38,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-//Route::middleware( 'auth:sanctum' )->group( function () {
+Route::middleware( 'auth:sanctum' )->group( function () {
 	Route::get( '/user', function ( Request $request ) {
 		return $request->user();
 	} );
@@ -194,7 +194,7 @@ use Illuminate\Support\Facades\Route;
 
 	//finance report
 	Route::get( '/getFinanceReport', [ FinanceController::class, 'getAccountStatement' ] );
-//} );
+} );
 
 
 Route::get( '/get-application-settings', [ ApplicationSettingsController::class, 'getApplicationSettings' ] );
